f3e7b7d3092c430279462b9b54077fe971141447
chriswood <caw@heapingbits.net>
2023-06-02 08:10:32 -0400

    Update formatting scripts

--- scripts/format_test_vectors.py
+++ scripts/format_test_vectors.py
@@ -20,43 +20,43 @@ def format_vector(vector_keys, vector_fname):
             formatted = formatted + "\n"
         print(formatted + "~~~\n")
 
-if "ed25519-blinding" in sys.argv[1]:
+if "type3-ed25519-blinding" in sys.argv[1]:
     ordered_keys = [
         "skS", "pkS", "bk", "pkR", "message", "context", "signature",
     ]
     format_vector(ordered_keys, sys.argv[1])
 
-if "ecdsa-blinding" in sys.argv[1]:
+if "type3-ecdsa-blinding" in sys.argv[1]:
     ordered_keys = [
         "skS", "pkS", "bk", "pkR", "message", "context", "signature",
     ]
     format_vector(ordered_keys, sys.argv[1])
 
-if "basic-public-issuance" in sys.argv[1]:
+if "type2-issuance" in sys.argv[1]:
     ordered_keys = [
         "skS", "pkS", "token_challenge", "nonce", "blind", "salt", "token_request", "token_response", "token"
     ]
     format_vector(ordered_keys, sys.argv[1])
 
-if "batched-private-issuance" in sys.argv[1]:
+if "typeF91A-issuance" in sys.argv[1]:
     ordered_keys = [
         "skS", "pkS", "token_challenge", "nonces", "blinds", "salt", "token_request", "token_response", "tokens"
     ]
     format_vector(ordered_keys, sys.argv[1])
 
-if "basic-private-issuance" in sys.argv[1]:
+if "type1-issuance" in sys.argv[1]:
     ordered_keys = [
         "skS", "pkS", "token_challenge", "nonce", "blind", "token_request", "token_response", "token"
     ]
     format_vector(ordered_keys, sys.argv[1])
 
-if "origin-encryption-test-vectors.json" in sys.argv[1]:
+if "type3-origin-encryption-test-vectors.json" in sys.argv[1]:
     ordered_keys = [
         "origin_name", "kem_id", "kdf_id", "aead_id", "issuer_encap_key_seed", "issuer_encap_key", "token_type", "token_key_id", "blinded_msg", "request_key", "issuer_encap_key_id", "encrypted_token_request"
     ]
     format_vector(ordered_keys, sys.argv[1])
 
-if "anon-origin-id-test-vectors.json" in sys.argv[1]:
+if "type3-anon-origin-id-test-vectors.json" in sys.argv[1]:
     ordered_keys = [
         "sk_sign", "pk_sign", "sk_origin", "request_blind", "request_key", "index_key", "anon_issuer_origin_id"
     ]
